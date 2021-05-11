63258a333f5b324caecca0f038a58d15c6077b3b
minibig <minibig@localhost>
2021-05-11 23:08:48 +0800

    by using hashmap implemantation

---
+++ Test1/src/leetcode/HashMapTwoSum.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTwoSum {
+
+	/**
+	 * https://leetcode.com/problems/two-sum/submissions/
+	 */
+	public static int[] twoSum(int[] nums, int target) {
+        
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i = 0; i < nums.length; i++) {
+			if(map.containsKey(target-nums[i])) {
+				return new int[] {map.get(target-nums[i]).intValue(), i};
+			}else {
+				map.put(nums[i], i);
+			}
+		}
+		return null;
+	}
+}
