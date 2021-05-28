4903d6b5218d40a10b293cb991d8aff902e070a3
minibig <minibig@localhost>
2021-05-28 22:09:21 +0800

    Maybe this gonna be better than previous version

--- Test1/src/leetcode/LeetCode28.java
+++ Test1/src/leetcode/LeetCode28.java
@@ -6,19 +6,15 @@ package leetcode;
  *
  */
 public class LeetCode28 {
-
 	public int strStr(String haystack, String needle) {
         
-        if(!haystack.contains(needle)){
-            return -1;
-        }else{
-        	int len = needle.length();
-        	for(int i = 0; i <= haystack.length() - len; i++) {
-        		if(haystack.substring(i, i + len).equals(needle)) {
-        			return i;
-        		}
-        	}
-            return 0;
+        int len = needle.length();
+        if(len == 0) return 0;
+        for(int i = 0; i <= haystack.length() - len; i++) {
+            if(haystack.substring(i, i + len).equals(needle)) {
+                return i;
+            }
         }
+        return -1;
     }
 }
