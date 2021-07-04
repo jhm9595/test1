2bdd596bf6573afe3b907ca53b590a6e35ae3ea8
minibig <minibig@jeonhyecbookpro>
2021-07-05 01:42:20 +0800

    LeetCode108

---
+++ Test1/src/leetcode/LeetCode108.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import commonclass.TreeNode;
+
+/**
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+ * @author minibig
+ *
+ */
+public class LeetCode108 {
+	
+	public static TreeNode sortedArrayToBST(int[] nums) {
+		
+		return setNextNode(nums, 0, nums.length - 1, new boolean[nums.length]);
+    }
+	
+	private static TreeNode setNextNode(int[] nums, int begin, int end, boolean[] check) {
+		
+		int mid = (begin + end) / 2;
+		
+		if(begin >= end && !check[mid]) {
+			return new TreeNode(nums[begin]);
+		}
+		
+		if(mid >= nums.length || check[mid]) {
+			return null;
+		}
+		
+		check[mid] = true;
+		
+		return new TreeNode(nums[mid], setNextNode(nums, begin, mid, check), setNextNode(nums, mid + 1, end, check));
+	}
+}
